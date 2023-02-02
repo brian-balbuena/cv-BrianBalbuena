f532b07dbb479adfe18518054b4e5929c0b3a3ae
Brian Balbuena <brian.sir93@gmail.com>
2023-02-02 02:29:43 -0300

    ultimos detalles de JS

--- function.js
+++ function.js
@@ -1,25 +1,39 @@
 
 
-
-// document.getElementById('body').onscroll = function() {
-//     document.getElementById('textedu').style.color= 'red' ;
-//     console.log("hola")
-// }
-
-var conatador = 0 
-
- window.onscroll = function(){
-    conatador++; 
-    if(conatador > 3 ) {
-   document.getElementById('imgperfil').style.opacity= 0.5;
-     console.log('hola');
-    }
-
-    
- }
-
-
+// botones footer
 document.getElementById('email').addEventListener('click', function () {
-    
-     location.href="https://mail.google.com/mail/u/0/#inbox/FMfcgzGrcPKfHrmgrXTKZWpRDVHsFkTg?compose=DmwnWrRqgkDLLmTffsVMSTjMgDlpjZKwnfMgsgvddsHVgFNbzdGsmMWWCdRNrmWPTtgDqGCkKncg";
-})
+
+    document.getElementById('mailcontacto').classList.toggle('visible');
+});
+
+document.getElementById('whatsapp').addEventListener('click', function () {
+
+    document.getElementById('wppcontacto').classList.toggle('visibleA');
+});
+
+document.getElementById('linkedin').addEventListener('click', function () {
+
+    document.getElementById('incontacto').classList.toggle('visibleB');
+});
+
+// visuales
+
+document.getElementById('educacion').addEventListener('mouseover', function () {
+    document.getElementById('decoracion').style.animation = "expandir 4s ";
+});
+
+document.getElementById('botonmi').addEventListener('click', function () {
+    document.getElementById('acerA').classList.toggle('acermiAA')
+    document.getElementById('datospersonales').classList.toggle('datospersonalesA')
+
+   
+});
+
+
+//  if (document.getElementById('miB').classList.toggle('visualmi')) {
+//     document.getElementById('cortina').innerHTML = "abrir cortina";
+//  } else {
+//     document.getElementById('cortina').innerHTML = "cerrar ventana";
+//   }
+
+ 
